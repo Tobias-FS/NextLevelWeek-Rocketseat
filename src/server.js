25b988a10c14fdd485e3950bd4e475986169f5fd
Tobias Ferreira da Silva <tobias_f@outlook.com>
2020-08-23 17:58:23 -0300

    Template engine - configurando o nunjucks

--- src/server.js
+++ src/server.js
@@ -1,19 +1,53 @@
+const proffys = [
+    {
+        name: "Diego Fernades",
+        avatar:"htts://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsaap: "532653623562",
+        bio: "qualquer coisa",
+        subject: "Quimica",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Jozé",
+        avatar:"htts://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsaap: "532653623562",
+        bio: "qualquer coisa",
+        subject: "Quimica",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html")
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
+
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
-
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
 
-
+server
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500)
 
 // console.log(__dirname)
