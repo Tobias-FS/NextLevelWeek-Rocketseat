916e0fc926f17cf8a4f34a6434f76784e49c87d3
Tobias Ferreira da Silva <tobias_f@outlook.com>
2020-08-23 21:30:24 -0300

    Populando os campos dinamicamente

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const proffys = [
     {
         name: "Diego Fernades",
-        avatar:"htts://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
         whatsaap: "532653623562",
         bio: "qualquer coisa",
         subject: "Quimica",
@@ -12,7 +12,7 @@ const proffys = [
     },
     {
         name: "Jozé",
-        avatar:"htts://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
         whatsaap: "532653623562",
         bio: "qualquer coisa",
         subject: "Quimica",
@@ -22,16 +22,57 @@ const proffys = [
         time_to: [1220]
     }
 ]
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação fisica",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sabado",
+]
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber
+    return subjects[position]
+}
 function pageLanding(req, res) {
     return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html")
+    const filters = req.query
+    
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
+console.log()
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+    
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty) {
+        data.subject = getSubject(data.subject)
+
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+    return res.render("give-classes.html", {subjects, weekdays })
 }
 
 const express = require('express')
